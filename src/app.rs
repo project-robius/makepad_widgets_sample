d702bb3d3bb8207c45b71a73536958f2f1544c03
Edward Tan <guofoo@gmail.com>
2023-05-24 14:51:12 -0700

    add dropdown menu

--- src/app.rs
+++ src/app.rs
@@ -11,11 +11,133 @@ use makepad_widgets::*;
 live_design!{
     import makepad_widgets::button::Button;
     import makepad_widgets::desktop_window::DesktopWindow;
+    import makepad_widgets::drop_down::DropDown;
     import makepad_widgets::frame::Frame;
-    import makepad_widgets::label::Label;
     import makepad_widgets::frame::Image;
+    import makepad_widgets::label::Label;
     import makepad_widgets::text_input::TextInput;
+    import makepad_draw::shader::std::*;
+
+    FONT_SIZE_H2 = 9.5
     
+    SPACING_CONTROLS = 7.5;
+
+    SSPACING_0 = 0.0
+    SSPACING_1 = 4.0
+    SSPACING_2 = (SSPACING_1 * 2)
+    SSPACING_3 = (SSPACING_1 * 3)
+    SSPACING_4 = (SSPACING_1 * 4)
+    
+    SPACING_0 = {top: (SSPACING_0), right: (SSPACING_0), bottom: (SSPACING_0), left: (SSPACING_0)}
+    SPACING_1 = {top: (SSPACING_1), right: (SSPACING_1), bottom: (SSPACING_1), left: (SSPACING_1)}
+    SPACING_2 = {top: (SSPACING_2), right: (SSPACING_2), bottom: (SSPACING_2), left: (SSPACING_2)}
+    SPACING_3 = {top: (SSPACING_3), right: (SSPACING_3), bottom: (SSPACING_3), left: (SSPACING_3)}
+    SPACING_4 = {top: (SSPACING_4), right: (SSPACING_4), bottom: (SSPACING_4), left: (SSPACING_4)}
+    H2_TEXT_BOLD = {
+        font_size: (FONT_SIZE_H2),
+        font: {path: dep("crate://makepad-widgets/resources/IBMPlexSans-SemiBold.ttf")}
+    }
+    H2_TEXT_NORMAL = {
+        font_size: (FONT_SIZE_H2),
+        font: {path: dep("crate://makepad-widgets/resources/IBMPlexSans-Text.ttf")}
+    }
+    
+    COLOR_DOWN_FULL = #000
+    
+    COLOR_DOWN_0 = #x00000000
+    COLOR_UP_0 = #xFFFFFF00
+    COLOR_UP_5 = #xFFFFFF66
+
+    ElementBox = <Frame> {
+        draw_bg: {color: (COLOR_DOWN_0)}
+        walk: {width: Fill, height: Fit}
+        layout: {flow: Down, padding: <SPACING_1> {}, spacing: (SSPACING_1)}
+    }
+
+    FishDropDown = <DropDown> {
+        walk: {width: Fit}
+        layout: {padding: {top: (SSPACING_2), right: (SSPACING_4), bottom: (SSPACING_2), left: (SSPACING_2)}}
+        
+        draw_label: {
+            text_style: <H2_TEXT_NORMAL> {},
+            fn get_color(self) -> vec4 {
+                return mix(
+                    mix(
+                        mix(
+                            (#xFFF8),
+                            (#xFFF8),
+                            self.focus
+                        ),
+                        (#xFFFF),
+                        self.hover
+                    ),
+                    (#x000A),
+                    self.pressed
+                )
+            }
+        }
+        
+        popup_menu: {
+            menu_item: {
+                indent_width: 10.0
+                walk: {width: Fill, height: Fit}
+                
+                layout: {
+                    padding: {left: (SSPACING_4), top: (SSPACING_2), bottom: (SSPACING_2), right: (SSPACING_4)},
+                }
+                
+                draw_bg: {
+                    color: #x48,
+                    color_selected: #x6
+                }
+            }
+        }
+        
+        draw_bg: {
+            fn pixel(self) -> vec4 {
+                let sdf = Sdf2d::viewport(self.pos * self.rect_size);
+                self.get_bg(sdf);
+                // triangle
+                let c = vec2(self.rect_size.x - 10.0, self.rect_size.y * 0.5)
+                let sz = 2.5;
+                
+                sdf.move_to(c.x - sz, c.y - sz);
+                sdf.line_to(c.x + sz, c.y - sz);
+                sdf.line_to(c.x, c.y + sz * 0.75);
+                sdf.close_path();
+                
+                sdf.fill(mix(#FFFA, #FFFF, self.hover));
+                
+                return sdf.result
+            }
+            
+            fn get_bg(self, inout sdf: Sdf2d) {
+                sdf.rect(
+                    0,
+                    0,
+                    self.rect_size.x,
+                    self.rect_size.y
+                )
+                sdf.fill((COLOR_UP_0))
+            }
+        }
+    }
+
+
+    InstrumentDropdown = <ElementBox> {
+        layout: {align: {y: 0.5}, padding: <SPACING_0> {}, flow: Right}
+        label = <Label> {
+            walk: {width: Fit}
+            draw_label: {
+                color: (COLOR_UP_5)
+                text_style: <H2_TEXT_BOLD>{},
+            }
+        }
+        dropdown = <FishDropDown> {
+            walk: {margin: {left: (SSPACING_1), right: (SSPACING_1)}}
+        }
+    }
+
     // The `{{App}}` syntax is used to inherit a DSL object from a Rust struct. This tells the
     // Makepad runtime that our DSL object corresponds to a Rust struct named `App`. Whenever an
     // instance of `App` is initialized, the Makepad runtime will obtain its initial values from
@@ -82,18 +204,14 @@ live_design!{
             
             button1 = <Button> {
                 icon_walk:{margin:{left:10}, width:16,height:Fit}
-                label: "Click to count"
+                label: "Click Button"
             }
-            // input1 = <TextInput> {
-            //     walk: {width: 100, height: 30},
-            //     label: "Click to count"
-            // }
-            // A label to display the counter.
+
             label1 = <Label> {
                 draw_label: {
                     color: #f
                 },
-                label: "Counter: 0"
+                label: "Label: 0"
             }
 
             InputFrame = <Frame> {
@@ -112,13 +230,53 @@ live_design!{
                     draw_label: {
                         color: #f
                     },
-                    label: "Input Field:"
+                    label: "Text Input:"
                 }
 
                 input_example = <TextInput> {
                     walk: {width:100, height:30},
                     text: "Enter Text Here"
                 }
+            }
+
+            DropDownFrame = <Frame> {
+                walk: {width: Fit, height: Fit}
+                layout: {
+                    flow: Right,
+                    spacing: 10,
+                },
+
+                label_example = <Label> {
+                    walk: {height:30},
+                    align: {
+                        // x: 1.5,
+                        y: 1
+                    }
+                    draw_label: {
+                        color: #f
+                    },
+                    label: "Dropdown:"
+                }
+
+                my_dropdown = <InstrumentDropdown> {
+                    layout: {flow: Down}
+                    walk: {
+                        width: Fit,
+                        height: 30,
+                        margin: {
+                            top: (SPACING_CONTROLS),
+                            right: (SPACING_CONTROLS),
+                            bottom: (SPACING_CONTROLS),
+                            left: 0.0
+                        }
+                    }
+                    dropdown = {
+                        values: [sel1, sel2, sel3, sel4]
+                        labels: ["Selection 1", "Selection 2", "Selection 3","Selection 4"]
+                    }
+                // }
+                }
+
             }
 
         }
@@ -152,7 +310,6 @@ pub struct App {
     // The #[rust] attribute here is used to indicate that this field should *not* be initialized
     // from a DSL object, even when a corresponding property exists.
     #[rust] counter: usize,
-    #[rust] sample: String,
 
 }
 
@@ -163,10 +320,14 @@ impl LiveHook for App {
 }
 
 impl App {
-    async fn _do_network_request(_cx:CxRef, _ui:WidgetRef, _url:&str)->String{
-        //let x = fetch(urL).await;
-        //ui.get_label(id!(thing)).set_text(&mut *cx.borrow_mut(), x);
-        "".to_string()
+    // async fn _do_network_request(_cx:CxRef, _ui:WidgetRef, _url:&str)->String{
+    //     //let x = fetch(urL).await;
+    //     //ui.get_label(id!(thing)).set_text(&mut *cx.borrow_mut(), x);
+    //     "".to_string()
+    // }
+
+    pub fn data_bind(&mut self, mut db: DataBindingMap) {
+        db.bind(id!(my_dropdown), ids!(my_dropdown.dropdown));
     }
 }
 
@@ -176,7 +337,7 @@ impl AppMain for App {
     fn handle_event(&mut self, cx: &mut Cx, event: &Event) {
         // println!("entering handle_event");
         if let Event::Draw(event) = event {
-            println!("  draw = event");
+            println!("  immediate draw = event");
             // This is a draw event, so create a draw context and use that to draw our application.
             return self.ui.draw_widget_all(&mut Cx2d::new(cx, event));
         }
@@ -189,7 +350,7 @@ impl AppMain for App {
         // Get a reference to our button from the frame, and check if one of the actions returned by
         // the frame was a notification that the button was clicked.
         if self.ui.get_button(id!(button1)).clicked(&actions) {
-            println!("  button1 clicked");
+            println!("  retained = button");
 
             //cx.spawn_async(Self::do_network_request(cx.get_ref(), self.ui.clone()))
             // Increment the counter.
@@ -198,7 +359,7 @@ impl AppMain for App {
             // Get a reference to our label from the frame, update its text, and schedule a redraw
             // for it.
             let label = self.ui.get_label(id!(label1));
-            label.set_label(&format!("Counter: {}", self.counter));
+            label.set_label(&format!("Label: {}", self.counter));
             label.redraw(cx);
         }
 
@@ -221,7 +382,10 @@ impl AppMain for App {
         //         }
         //     }
         // }
-
+        let ui = self.ui.clone();
+        let mut drop_db = DataBindingStore::new();
+        self.data_bind(drop_db.widgets_to_data(cx, &actions, &ui));
+        self.data_bind(drop_db.data_to_widgets(cx, &actions, &ui));
 
     }
 }
